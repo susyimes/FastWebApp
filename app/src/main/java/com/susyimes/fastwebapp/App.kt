3ee1eafe5b5c7df08628264947195204fd62969f
susyimes <susyimes@gmaill.com>
2021-05-20 16:36:55 +0800

    add umeng

--- app/src/main/java/com/susyimes/fastwebapp/App.kt
+++ app/src/main/java/com/susyimes/fastwebapp/App.kt
@@ -1,11 +1,15 @@
 package com.susyimes.fastwebapp
 
 import android.app.Application
+import com.umeng.analytics.MobclickAgent
+import com.umeng.commonsdk.UMConfigure
 
 class App :Application() {
 
     override fun onCreate() {
         super.onCreate()
         ObjectBox.init(this)
+        UMConfigure.init(this, "60a61ecfc9aacd3bd4df4ada", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "")
+        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
     }
 }
